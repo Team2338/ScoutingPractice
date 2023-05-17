90b9bb46fd4870554106ca4c8284ea5cc75d36c9
Patrick Ubelhor <pubelhor@gmail.com>
2023-05-17 14:37:06 -0500

    More grid work

--- 2023/src/components/collection-page/grid/Grid.tsx
+++ 2023/src/components/collection-page/grid/Grid.tsx
@@ -2,10 +2,23 @@ import React from 'react';
 import './Grid.scss';
 import { activateNode, useAppDispatch, useAppSelector } from '../../../state';
 import { Gamemode } from '../../../models';
+import { Button } from '@mui/material';
 
 
 interface IProps {
 	gamemode: Gamemode
+}
+
+function getSymbol(index: number) {
+	if (index > 17) {
+		return '▲ ■';
+	}
+
+	if (index % 3 === 1) {
+		return '■';
+	}
+
+	return '▲';
 }
 
 export default function Grid(props: IProps) {
@@ -22,16 +35,31 @@ export default function Grid(props: IProps) {
 
 	const elements = combinedGrid.map((numPieces: number, index: number) => {
 		let color: 'primary' | 'secondary' | 'success';
-		let variant: 'outlined' | 'filled';
+		let variant: 'outlined' | 'contained';
 
 		if (props.gamemode === Gamemode.auto) {
-			color = 'primary';
+			color = (autoGrid[index] === 0) ? 'primary' : 'secondary';
+		} else {
+			if (combinedGrid[index] === 0) {
+				color = 'primary';
+			} else if (combinedGrid[index] === 1)
 		}
+
+		return (
+			<Button
+				key={index}
+				color={color}
+				variant={variant}
+				onClick={() => handleClick(index)}
+			>
+				{ getSymbol(index) }
+			</Button>
+		);
 	});
 
 	return (
 		<div className="grid-wrapper">
-			Grid Component
+			{ elements }
 		</div>
 	);
 }
